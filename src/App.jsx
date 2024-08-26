5cfb9c36741d0ee57cf9ba39f9a7afc3770e580b
tgunnr <finalresidentevilfantasy@gmail.com>
2024-08-26 04:41:15 -0700

    update UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,113 @@
 // src/App.jsx
+import { useState } from 'react'
+
+import './App.css'
+
+const team = []
+const money = 100
+const zombieFighters = [
+  {
+    name: 'Survivor',
+    price: 12,
+    strength: 6,
+    agility: 4,
+    img: 'https://via.placeholder.com/150/92c952',
+  },
+  {
+    name: 'Scavenger',
+    price: 10,
+    strength: 5,
+    agility: 5,
+    img: 'https://via.placeholder.com/150/771796',
+  },
+  {
+    name: 'Shadow',
+    price: 18,
+    strength: 7,
+    agility: 8,
+    img: 'https://via.placeholder.com/150/24f355',
+  },
+  {
+    name: 'Tracker',
+    price: 14,
+    strength: 7,
+    agility: 6,
+    img: 'https://via.placeholder.com/150/d32776',
+  },
+  {
+    name: 'Sharpshooter',
+    price: 20,
+    strength: 6,
+    agility: 8,
+    img: 'https://via.placeholder.com/150/1ee8a4',
+  },
+  {
+    name: 'Medic',
+    price: 15,
+    strength: 5,
+    agility: 7,
+    img: 'https://via.placeholder.com/150/66b7d2',
+  },
+  {
+    name: 'Engineer',
+    price: 16,
+    strength: 6,
+    agility: 5,
+    img: 'https://via.placeholder.com/150/56acb2',
+  },
+  {
+    name: 'Brawler',
+    price: 11,
+    strength: 8,
+    agility: 3,
+    img: 'https://via.placeholder.com/150/8985dc',
+  },
+  {
+    name: 'Infiltrator',
+    price: 17,
+    strength: 5,
+    agility: 9,
+    img: 'https://via.placeholder.com/150/392537',
+  },
+  {
+    name: 'Leader',
+    price: 22,
+    strength: 7,
+    agility: 6,
+    img: 'https://via.placeholder.com/150/602b9e',
+  },
+];
+
+
+
 
 const App = () => {
+  
+  const Fighters = (props) => {
+    const { name, price, strength, agility, img } = props
+    return (
+      <>
+            <li key={name}>
+              <p>{name}</p>
+              <p>{price}</p>
+              <p>{strength}</p>
+              <p>{agility}</p>
+              <p><a href={img}/></p>
+            </li>
+      </>
+    )
+  }
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <section>
+        <ul>
+        {zombieFighters.map((fighter) => (
+          <Fighters key={fighter.name} {...fighter}/>
+        ))}
+        </ul>
+      </section>
+    </>
   );
 }
 
